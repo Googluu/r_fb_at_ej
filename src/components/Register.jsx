5ee4de1f30061cdb671e20c10a00923754b8cf6f
Sneyder <sneyder.h.rodriguez@gmail.com>
2022-11-15 17:16:49 -0500

    04 step: register user

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,50 @@
+import { useState } from 'react';
+import { useAuth } from '../context/authContext';
+import { useNavigate } from 'react-router-dom';
+
 export const Register = () => {
+
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
+  const { signup } = useAuth();
+  const navigate = useNavigate();
+  const [error, setError] = useState();
+
+  const handleChange = ({ target: { name, value } }) => {
+    setUser({ ...user, [name]: value })
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setError('');
+    try {
+      await signup(user.email, user.password);
+      navigate('/')
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
   return (
-    <div>Register</div>
+    <div>
+
+      {error && <p>{error}</p>}
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" placeholder="youremail@company.ltd"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" placeholder="******" id="password"
+          onChange={handleChange}
+        />
+
+        <button>Register</button>
+      </form>
+    </div>
   )
 };
--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,21 +1,22 @@
 import { createContext, useContext } from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+
+import { auth } from '../firebase.config';
 
 export const authContext = createContext();
 
 export const useAuth = () => {
   const context = useContext(authContext);
-  if (!context) throw new Error('No auth context');
+  if (!context) throw new Error("There isn't auth provider");
   return context;
 };
 
 export function AuthProvider({ children }) {
 
-  const user = {
-    login: true
-  }
+  const signup = (email, password) => createUserWithEmailAndPassword(auth, email, password);
 
   return (
-    <authContext.Provider value={{ user }}>
+    <authContext.Provider value={{ signup }}>
       {children}
     </authContext.Provider>
   )
