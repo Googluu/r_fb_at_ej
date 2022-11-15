4e6b4c33c2d1007dd71330a6e8b8c1917d94ac00
Sneyder <sneyder.h.rodriguez@gmail.com>
2022-11-15 15:27:32 -0500

    03 step: context

---
+++ src/context/authContext.js
@@ -0,0 +1,22 @@
+import { createContext, useContext } from 'react';
+
+export const authContext = createContext();
+
+export const useAuth = () => {
+  const context = useContext(authContext);
+  if (!context) throw new Error('No auth context');
+  return context;
+};
+
+export function AuthProvider({ children }) {
+
+  const user = {
+    login: true
+  }
+
+  return (
+    <authContext.Provider value={{ user }}>
+      {children}
+    </authContext.Provider>
+  )
+}
